f41c6553646c248f898592e9a68e8ec4c6462c5d
Tripon Eugen <tripon.eugen@gmail.com>
2019-11-07 15:27:33 +0200

    create release

--- release.py
+++ release.py
@@ -2,7 +2,43 @@
 
 import shutil
 import subprocess
+import json
+import os
 from decimal import Decimal
+from urlparse import urlparse
+
+
+def run_command(command):
+    p = subprocess.Popen(command, stdout=subprocess.PIPE)
+    (response, err) = p.communicate()
+    p_status = p.wait()
+    return (response, err)
+
+
+def _tag_release(version):
+    if os.path.isfile('./token'):
+        with open('./token') as f:
+            read_data = f.read()
+        token = read_data.strip()
+    else:
+        print "Could not make release: token not found"
+        return
+
+    payload = {
+        "target_commitish": "master",
+        'tag_name': 'v' + str(version),
+        'name': 'v' + str(version),
+        'body': 'Release ' + str(version),
+    }
+
+    (url, err) = run_command(['git', 'config', '--get', 'remote.origin.url'])
+    data = urlparse(url)
+    path = data.path.replace('.git', '')
+    url = 'https://api.github.com/repos' + path.strip() + '/releases'
+    (resp, err) = run_command(['curl', '-H', 'Authorization: token '+str(token), url, '-X', 'POST', '-d', json.dumps(payload) ])
+    data = json.loads(resp)
+    if 'message' in data:
+        print "Release error:" + data['message']
 
 
 def release():
@@ -35,6 +71,8 @@ def release():
                      'Release {}'.format(version)])
     subprocess.call(['git', 'push', '--tags'])
     subprocess.call(['git', 'push'])
+
+    _tag_release(version)
 
 
 if __name__ == "__main__":
